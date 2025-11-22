e494364d041bdcd86b97a3cb46ec43b13229e0f7
Mengmeng WU <71432597+mengmeng327@users.noreply.github.com>
2025-11-22 18:48:56 +0800

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,12 @@
-module.exports = {
-  content: ["./index.html", "./src/**/*.{ts,tsx}"],
-  theme: { extend: {} },
-  plugins: []
-};
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    // 关键：必须包含 src 下的所有 tsx/ts 文件
+    "./src/**/*.{js,ts,jsx,tsx}", 
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
