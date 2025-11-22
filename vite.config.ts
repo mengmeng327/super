6ee4017f81bda31a32eae1e45ed3195b7cf1d186
Mengmeng WU <71432597+mengmeng327@users.noreply.github.com>
2025-11-22 15:28:32 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,34 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import { VitePWA } from "@vite-pwa/plugin";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
+
 
 export default defineConfig({
   plugins: [
     react(),
     VitePWA({
-      registerType: "autoUpdate",
-      includeAssets: ["favicon.ico"],
-      manifest: true
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico'],
+      manifest: {
+        name: 'Super App',
+        short_name: 'Super',
+        description: 'A PWA version of the app',
+        theme_color: '#ffffff',
+        start_url: '/',
+        display: 'standalone',
+        icons: [
+          {
+            src: 'icons/icon-192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'icons/icon-512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
     })
   ]
-});
+})
